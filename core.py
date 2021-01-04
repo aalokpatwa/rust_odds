5213458d266b07a92f80e9fc72e8b30cabcc39a3
Aalok Patwa <49799004+aalokpatwa@users.noreply.github.com>
2021-01-03 21:25:26 -0800

    Update core.py
    fixed degrees of freedom bug. increased spins to 1000

--- core.py
+++ core.py
@@ -1,5 +1,8 @@
 import random
 import numpy as np
+import time
+
+beginning = time.perf_counter()
 
 
 P_YELLOW = 0.48
@@ -10,7 +13,7 @@ P_RED = 0.04
 
 options = ["y", "g", "b", "p", "r"]
 
-num_spins = 100
+num_spins = 1000
 successes = 0
 
 
@@ -20,37 +23,38 @@ scrap_allocation = np.array([0.67, 0.33, 0, 0 ,0])
 best_rate = 0
 best_allocation = None
 
-for y in np.arange(0, 1, 0.1):
-    for g in np.arange(0, 1-y, 0.1):
-        for b in np.arange(0, 1-(y+g), 0.1):
-            for p in np.arange(0, 1-(y+g+b), 0.1):
-                for r in np.arange(0, 1-(y+g+b+p), 0.1):
-                    scrap_allocation = np.array([y, g, b, p, r])
-                    successes = 0
-                    for spin in range(num_spins):
-                        num_betted = 100
-                        bet = num_betted * scrap_allocation
+for y in np.arange(0, 1, 0.05):
+    for g in np.arange(0, 1-y, 0.05):
+        for b in np.arange(0, 1-(y+g), 0.05):
+            for p in np.arange(0, 1-(y+g+b), 0.05):
+                r = 1-(y+g+b+p)
+                scrap_allocation = np.array([y, g, b, p, r])
+                successes = 0
+                for spin in range(num_spins):
+                    num_betted = 100
+                    bet = num_betted * scrap_allocation
 
-                        revenue = 0
-                        selection = np.random.choice(options, size=1, p=np.array([P_YELLOW, P_GREEN, P_BLUE, P_PURPLE, P_RED]))[0]
-                        if selection == "y":
-                            revenue += (bet[0] * 2)
-                        elif selection == "g":
-                            revenue += (bet[1] * 4)
-                        elif selection == "b":
-                            revenue += (bet[2] * 6)
-                        elif selection == "p":
-                            revenue += (bet[3] * 12)
-                        elif selection == "r":
-                            revenue += (bet[4] * 25)
-                        if revenue > num_betted:
-                            successes += 1
+                    revenue = 0
+                    selection = np.random.choice(options, size=1, p=np.array([P_YELLOW, P_GREEN, P_BLUE, P_PURPLE, P_RED]))[0]
+                    if selection == "y":
+                        revenue += (bet[0] * 2)
+                    elif selection == "g":
+                        revenue += (bet[1] * 4)
+                    elif selection == "b":
+                        revenue += (bet[2] * 6)
+                    elif selection == "p":
+                        revenue += (bet[3] * 12)
+                    elif selection == "r":
+                        revenue += (bet[4] * 25)
+                    if revenue > num_betted:
+                        successes += 1
+                success_rate = successes / num_spins
+                if success_rate > best_rate:
+                    best_allocation = scrap_allocation
+                    best_rate = success_rate
+end = time.perf_counter()
 
-                    success_rate = successes / num_spins
-                    if success_rate > best_rate:
-                        best_allocation = scrap_allocation
-                        best_rate = success_rate
-
+print (end-beginning)
 print (best_allocation)
 print (best_rate)
 
